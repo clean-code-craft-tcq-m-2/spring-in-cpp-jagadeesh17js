efd6229ec1f072456ecaef9bed4a8890e1f7f6a1
jagadeesh17js <116014892+jagadeesh17js@users.noreply.github.com>
2022-10-22 20:38:11 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -11,13 +11,33 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& vari) {
     float x = 0;
     Stats var ;
 //     var.average = vari.at(0);
-    if (isnan(accumulate( vari.begin(), vari.end(), 0.0)/ vari.size()) == 0)
+//     if (isnan(accumulate( vari.begin(), vari.end(), 0.0)/ vari.size()) == 0)
+//     {
+//         var.average = accumulate( vari.begin(), vari.end(), 0.0)/ vari.size();
+//     }
+    
+    
+    
+    auto sum = accumulate( vari.begin(), vari.end(), 0.0);
+    if (isnan(sum/vari.size()) == 0 )
     {
-        var.average = accumulate( vari.begin(), vari.end(), 0.0)/ vari.size();
+        var.average = sum/vari.size();
     }
+    else {var.average = 0}
+    
+    
+    
+    
+//     var.average = isnan
+//     if (isnan(accumulate( vari.begin(), vari.end(), 0.0)/ vari.size()) == 0)
+//     {
+//         var.average = accumulate( vari.begin(), vari.end(), 0.0)/ vari.size();
+//     }
     
 //     std::cout << "var.average = " << var.average << "\n";
 //     std::vector<float>::iterator result;
+    
+    
     auto result = std::max_element(vari.begin(), vari.end());
     if (isnan(*result) == 0)
     {
