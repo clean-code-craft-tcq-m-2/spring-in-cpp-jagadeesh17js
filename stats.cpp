6c7f5c5519cb4ec0bbdb871ce1d9c95112634a5e
jagadeesh17js <116014892+jagadeesh17js@users.noreply.github.com>
2022-10-22 22:36:17 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -8,13 +8,7 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& vari) {
     //Implement statistics here
-//     float x = 0;
     Stats var ;
-//     var.average = vari.at(0);
-//     if (isnan(accumulate( vari.begin(), vari.end(), 0.0)/ vari.size()) == 0)
-//     {
-//         var.average = accumulate( vari.begin(), vari.end(), 0.0)/ vari.size();
-//     }
     
     if (vari.size() == 0)
     {
@@ -36,43 +30,4 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& vari) {
     }
     
     return var;
-    
-//     if (isnan(sum/vari.size()) == 0 )
-//     {
-//         var.average = sum/vari.size();
-//     }
-//     else {var.average = 0;}
-    
-    
-    
-    
-//     var.average = isnan
-//     if (isnan(accumulate( vari.begin(), vari.end(), 0.0)/ vari.size()) == 0)
-//     {
-//         var.average = accumulate( vari.begin(), vari.end(), 0.0)/ vari.size();
-//     }
-    
-//     std::cout << "var.average = " << var.average << "\n";
-//     std::vector<float>::iterator result;
-    
-    
-//     auto result = std::max_element(vari.begin(), vari.end());
-//     if (isnan(*result) == 0)
-//     {
-//        var.max = *result; 
-//     }
-//     var.max = *result;
-// //     std::cout << "var.max = " << var.max << "\n";
-    
-    
-//      auto result1 = std::min_element(vari.begin(), vari.end());
-//     if (isnan(*result1) == 0)
-//     {
-//        var.min = *result1;
-//     }
-    
-//     std::cout << "var.min = " << var.min << "\n";
-    
-//     var.min = std::min_element(vari.begin(), vari.end());
-//     return var;
 }
