ebcb9f9b1d17126a1e0044575da32e2174c77221
jagadeesh17js <116014892+jagadeesh17js@users.noreply.github.com>
2022-10-22 20:06:48 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -18,6 +18,10 @@ TEST_CASE("average is NaN for empty array") {
     auto computedStats = Statistics::ComputeStatistics({});
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
+    
+    std::cout << "computedStats.average = " << computedStats.average << std::endl;
+    std::cout << "computedStats.max = " << computedStats.max << std::endl;
+    std::cout << "computedStats.min = " << computedStats.min << std::endl;
     isnan(computedStats.average);
     isnan(computedStats.max);
     isnan(computedStats.min);
