00ae7b34e640c8a437219f5c8d77583d42a5c1e9
jagadeesh17js <116014892+jagadeesh17js@users.noreply.github.com>
2022-10-22 19:05:15 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,7 +4,8 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& vari) {
     //Implement statistics here
     float x = 0;
     Stats var ;
-    var.average = vari.at(0);
+//     var.average = vari.at(0);
+    var.average = accumulate( vari.begin(), vari.end(), 0.0/ vari.size());
     var.max = x;
     var.min = x;
     return var;
