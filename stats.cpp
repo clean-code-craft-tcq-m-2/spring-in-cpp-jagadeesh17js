b66156374423f3ea04555e70ce8030e6f2cf8f96
jagadeesh17js <116014892+jagadeesh17js@users.noreply.github.com>
2022-10-22 18:07:47 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,4 +2,10 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
     //Implement statistics here
+    float dummy x = 0;
+    Stats var ;
+    var.average = x;
+    var.max = x;
+    var.min = x;
+    return var;
 }
